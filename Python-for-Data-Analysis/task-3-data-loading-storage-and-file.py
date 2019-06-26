64f209bedc6ef10c3ed8aadd1c00b4c21d26054e
Lingmoumou <hyuan.feng@gmail.com>
2019-06-26 23:05:17 +0800

    add task3

---
+++ Python-for-Data-Analysis/task-3-data-loading-storage-and-file.py
@@ -0,0 +1,12 @@
+#%% [markdown]
+# #Data Loading, Storage, and File Formats
+# #数据加载、存储与文件格式
+
+#%%
+import pandas as pd
+from pandas import Series,DataFrame
+
+#%%
+df = pd.read_csv('examples/ex1.csv')
+
+#%%
